44d57f897506de1c20eb4f2054cfb65abaabc13b
Alex <kozlov.alex.dev@gmail.com>
2023-04-25 11:16:39 +0300

    ApiService + Factory implementation

--- app/src/main/java/com/countlesswrongs/cryptomonitor/MainActivity.kt
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.countlesswrongs.cryptomonitor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.countlesswrongs.cryptomonitor.model.api.ApiFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private var compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val subscribe = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,EOS")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_DATA", it.toString())
+            }, {
+                Log.d("TEST_DATA", it.message.toString())
+            })
+        compositeDisposable.add(subscribe)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
+    }
+
 }
---
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/model/api/ApiService.kt
@@ -0,0 +1,37 @@
+package com.countlesswrongs.cryptomonitor.model.api
+
+import Secret
+import com.countlesswrongs.cryptomonitor.model.detailedresponse.CoinPriceInfoRawData
+import com.countlesswrongs.cryptomonitor.model.listresponse.CoinInfoListOfData
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = Secret.API_KEY,
+        @Query(QUERY_PARAM_LIMIT) limit: Int = LIMIT,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = Secret.API_KEY,
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
+    ): Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+
+        private const val CURRENCY = "USD"
+        private const val LIMIT = 10
+    }
+
+}
