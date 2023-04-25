0278cacbbefc6dca1d88acf6f4bd3561c132b313
Alex <kozlov.alex.dev@gmail.com>
2023-04-25 09:52:17 +0300

    POJO introduction

---
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/model/detailedresponse/CoinPriceInfoRawData.kt
@@ -0,0 +1,13 @@
+package com.countlesswrongs.cryptomonitor.model.detailedresponse
+
+import com.google.gson.JsonObject
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinPriceInfoRawData(
+
+    @SerializedName("RAW")
+    @Expose
+    val coinPriceInfoJsonObject: JsonObject? = null
+
+)
---
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/model/listresponse/CoinInfo.kt
@@ -0,0 +1,25 @@
+package com.countlesswrongs.cryptomonitor.model.listresponse
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+data class CoinInfo(
+
+    @SerializedName("Id")
+    @Expose
+    val id: String? = null,
+
+    @SerializedName("Name")
+    @Expose
+    val name: String? = null,
+
+    @SerializedName("FullName")
+    @Expose
+    val fullName: String? = null,
+
+    @SerializedName("ImageUrl")
+    @Expose
+    val imageUrl: String? = null
+
+)
