2c490b06e0dea7c2c69ccedeadbd2bd0ee7fa996
Alex <kozlov.alex.dev@gmail.com>
2023-05-22 13:30:35 +0300

    repository impl + refactor of Dao

--- app/src/main/java/com/countlesswrongs/cryptomonitor/data/database/dao/CoinInfoDao.kt
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/data/database/dao/CoinInfoDao.kt
@@ -5,18 +5,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.countlesswrongs.cryptomonitor.data.network.model.detailedresponse.CoinInfoDto
+import com.countlesswrongs.cryptomonitor.data.database.model.CoinInfoDbModel
 
 @Dao
 interface CoinInfoDao {
 
     @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
-    fun getPriceList(): LiveData<List<CoinInfoDto>>
+    fun getPriceList(): LiveData<List<CoinInfoDbModel>>
 
     @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
-    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinInfoDto>
+    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinInfoDbModel>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertPriceList(priceList: List<CoinInfoDto>)
+    fun insertPriceList(priceList: List<CoinInfoDbModel>)
 
 }
---
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package com.countlesswrongs.cryptomonitor.data.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.map
+import com.countlesswrongs.cryptomonitor.data.database.AppDatabase
+import com.countlesswrongs.cryptomonitor.data.mapper.*
+import com.countlesswrongs.cryptomonitor.domain.entity.CoinInfoEntity
+import com.countlesswrongs.cryptomonitor.domain.repository.CoinRepository
+
+class CoinRepositoryImpl(
+    private val application: Application
+    ): CoinRepository {
+
+    private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
+    private val mapper = CoinMapper()
+
+    override fun getCoinInfoList(): LiveData<List<CoinInfoEntity>> {
+        return coinInfoDao.getPriceList().map { modelList ->
+            modelList.map {
+                mapper.mapDbModelToEntity(it)
+            }
+        }
+    }
+
+    override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfoEntity> {
+        return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
+            mapper.mapDbModelToEntity(it)
+        }
+    }
+}
