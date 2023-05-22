525ef9fc7bb8ff98059e747c54baeb9984433d86
Alex <kozlov.alex.dev@gmail.com>
2023-05-22 17:18:10 +0300

    Fragment impl

--- app/src/main/java/com/countlesswrongs/cryptomonitor/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/presentation/CoinDetailActivity.kt
@@ -5,14 +5,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import com.countlesswrongs.cryptomonitor.R
 import com.countlesswrongs.cryptomonitor.databinding.ActivityCoinDetailBinding
-import com.countlesswrongs.cryptomonitor.presentation.viewmodel.CoinViewModel
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: CoinViewModel
 
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
@@ -32,18 +28,11 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fSym = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL_STRING
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.getDetailedInfo(fSym).observe(this) {
-            with(binding) {
-                textViewFromSymbol.text = it.fromSymbol
-                textViewToSymbol.text = it.toSymbol
-                textViewPrice.text = it.price
-                textViewLowestPriceToday.text = it.lowDay
-                textViewHighestPriceToday.text = it.highDay
-                textViewLastMarket.text = it.lastMarket
-                textViewLastUpdate.text = it.lastUpdate
-                Picasso.get().load(it.imageUrl).into(imageViewLogo)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container_details, CoinDetailFragment.newInstance(fSym))
+                .commit()
         }
     }
 
