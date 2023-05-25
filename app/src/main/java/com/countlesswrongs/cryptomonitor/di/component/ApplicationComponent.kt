8911243f767a7218164950583cef9dd71f10b1c1
Alex <kozlov.alex.dev@gmail.com>
2023-05-25 09:27:14 +0300

    ApplicationScope impl

--- app/src/main/java/com/countlesswrongs/cryptomonitor/di/component/ApplicationComponent.kt
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/di/component/ApplicationComponent.kt
@@ -3,11 +3,13 @@ package com.countlesswrongs.cryptomonitor.di.component
 import android.app.Application
 import com.countlesswrongs.cryptomonitor.di.module.DataModule
 import com.countlesswrongs.cryptomonitor.di.module.ViewModelModule
+import com.countlesswrongs.cryptomonitor.di.scope.ApplicationScope
 import com.countlesswrongs.cryptomonitor.presentation.CoinDetailFragment
 import com.countlesswrongs.cryptomonitor.presentation.CoinPriceListActivity
 import dagger.BindsInstance
 import dagger.Component
 
+@ApplicationScope
 @Component(modules = [DataModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
--- app/src/main/java/com/countlesswrongs/cryptomonitor/presentation/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/countlesswrongs/cryptomonitor/presentation/viewmodel/ViewModelFactory.kt
@@ -2,9 +2,11 @@ package com.countlesswrongs.cryptomonitor.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.countlesswrongs.cryptomonitor.di.scope.ApplicationScope
 import javax.inject.Inject
 import javax.inject.Provider
 
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
     private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
